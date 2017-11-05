f89d974a441395008717ad4647fd4c9c6f754406
Cord Meados <meadosc@gmail.com>
2017-11-05 14:45:26 -0800

    Permissions work right now. Used code from demo

--- app/src/main/java/com/example/android/cs496sqlite/MainActivity.java
+++ app/src/main/java/com/example/android/cs496sqlite/MainActivity.java
@@ -18,35 +18,55 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import static com.google.android.gms.common.GooglePlayServicesUtil.getErrorDialog;
+
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private static final int LOCATION_PERMISSON_RESULT = 17;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private TextView mLatText;
+    private TextView mLonText;
+    private Location mLastLocation;
+    private LocationListener mLocationListener;
+    private static final int LOCATION_PERMISSON_RESULT = 17;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mLatText = (TextView) findViewById(R.id.lat_output);
+        mLonText = (TextView) findViewById(R.id.lon_output);
+        mLatText.setText("Activity Created");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null) {
+                    mLonText.setText(String.valueOf(location.getLongitude()));
+                    mLatText.setText(String.valueOf(location.getLatitude()));
+                } else {
+                    mLonText.setText("No Location Avaliable");
+                }
+            }
+        };
     }
 
-    //Next two methods set up permissions.
-    //onStart() connects to googleAPIClient
-    //onConnected requests permissions
     @Override
     protected void onStart() {
         mGoogleApiClient.connect();
+        mLatText.setText("Activity Started");
         super.onStart();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSON_RESULT);
-            return;
-        }
     }
 
     @Override
@@ -56,12 +76,50 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLatText.setText("onConnect");
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSON_RESULT);
+            mLonText.setText("Lacking Permissions");
+            return;
+        }
+        updateLocation();
+    }
+
+    @Override
     public void onConnectionSuspended(int i) {
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Dialog errDialog = GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), 0);
+        errDialog.show();
+        return;
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == LOCATION_PERMISSON_RESULT){
+            if(grantResults.length > 0){
+                updateLocation();
+            }
+        }
+    }
+
+    private void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(mLastLocation != null){
+            mLonText.setText(String.valueOf(mLastLocation.getLongitude()));
+            mLatText.setText(String.valueOf(mLastLocation.getLatitude()));
+        } else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,mLocationListener);
+        }
     }
 }
