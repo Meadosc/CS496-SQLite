a89d124b87b46054625214a93db8f35c9034b6f7
Cord Meados <meadosc@gmail.com>
2017-11-06 14:23:46 -0800

    changed INT to LAT

--- app/src/main/java/com/example/android/cs496sqlite/SQLiteActivity.java
+++ app/src/main/java/com/example/android/cs496sqlite/SQLiteActivity.java
@@ -40,7 +40,7 @@ public class SQLiteActivity extends AppCompatActivity {
                 if(mSQLDB != null){
                     ContentValues vals = new ContentValues();
                     vals.put(DBContract.DemoTable.COLUMN_NAME_DEMO_STRING, ((EditText)findViewById(R.id.sql_text_input)).getText().toString());
-                    vals.put(DBContract.DemoTable.COLUMN_NAME_DEMO_INT, ((EditText)findViewById(R.id.sql_int_input)).getText().toString());
+                    vals.put(DBContract.DemoTable.COLUMN_NAME_DEMO_LAT, ((EditText)findViewById(R.id.sql_int_input)).getText().toString());
                     mSQLDB.insert(DBContract.DemoTable.TABLE_NAME,null,vals);
                     populateTable();
                 } else {
@@ -62,12 +62,12 @@ public class SQLiteActivity extends AppCompatActivity {
                 }
                 mSQLCursor = mSQLDB.query(DBContract.DemoTable.TABLE_NAME,
                         new String[]{DBContract.DemoTable._ID, DBContract.DemoTable.COLUMN_NAME_DEMO_STRING,
-                                DBContract.DemoTable.COLUMN_NAME_DEMO_INT}, DBContract.DemoTable.COLUMN_NAME_DEMO_INT + " > ?", new String[]{"100"}, null, null, null);
+                                DBContract.DemoTable.COLUMN_NAME_DEMO_LAT}, DBContract.DemoTable.COLUMN_NAME_DEMO_LAT + " > ?", new String[]{"100"}, null, null, null);
                 ListView SQLListView = (ListView) findViewById(R.id.sql_list_view);
                 mSQLCursorAdapter = new SimpleCursorAdapter(this,
                         R.layout.sql_item,
                         mSQLCursor,
-                        new String[]{DBContract.DemoTable.COLUMN_NAME_DEMO_STRING, DBContract.DemoTable.COLUMN_NAME_DEMO_INT},
+                        new String[]{DBContract.DemoTable.COLUMN_NAME_DEMO_STRING, DBContract.DemoTable.COLUMN_NAME_DEMO_LAT},
                         new int[]{R.id.sql_listview_string, R.id.sql_listview_int},
                         0);
                 SQLListView.setAdapter(mSQLCursorAdapter);
@@ -89,7 +89,7 @@ class SQLiteExample extends SQLiteOpenHelper {
         db.execSQL(DBContract.DemoTable.SQL_CREATE_DEMO_TABLE);
 
         ContentValues testValues = new ContentValues();
-        testValues.put(DBContract.DemoTable.COLUMN_NAME_DEMO_INT, 42);
+        testValues.put(DBContract.DemoTable.COLUMN_NAME_DEMO_LAT, 42);
         testValues.put(DBContract.DemoTable.COLUMN_NAME_DEMO_STRING, "Hello SQLite");
         db.insert(DBContract.DemoTable.TABLE_NAME,null,testValues);
     }
@@ -108,17 +108,17 @@ final class DBContract {
         public static final String DB_NAME = "demo_db";
         public static final String TABLE_NAME = "demo";
         public static final String COLUMN_NAME_DEMO_STRING = "demo_string";
-        public static final String COLUMN_NAME_DEMO_INT = "demo_int";
-        public static final int DB_VERSION = 12;
+        public static final String COLUMN_NAME_DEMO_LAT = "demo_int";
+        public static final int DB_VERSION = 17;
 
 
         public static final String SQL_CREATE_DEMO_TABLE = "CREATE TABLE " +
                 DemoTable.TABLE_NAME + "(" + DemoTable._ID + " INTEGER PRIMARY KEY NOT NULL," +
                 DemoTable.COLUMN_NAME_DEMO_STRING + " VARCHAR(255)," +
-                DemoTable.COLUMN_NAME_DEMO_INT + " INTEGER);";
+                DemoTable.COLUMN_NAME_DEMO_LAT + " INTEGER);";
 
         public static final String SQL_TEST_DEMO_TABLE_INSERT = "INSERT INTO " + TABLE_NAME +
-                " (" + COLUMN_NAME_DEMO_STRING + "," + COLUMN_NAME_DEMO_INT + ") VALUES ('test', 123);";
+                " (" + COLUMN_NAME_DEMO_STRING + "," + COLUMN_NAME_DEMO_LAT + ") VALUES ('test', 123);";
 
         public  static final String SQL_DROP_DEMO_TABLE = "DROP TABLE IF EXISTS " + DemoTable.TABLE_NAME;
     }
